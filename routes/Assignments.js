ef93b55d6b175d9063bceced2769b0a499ebab47
Kratos2k7 <umernadeem321@gmail.com>
2024-04-12 21:07:27 +0500

    Refactor token handling in Assignments.js and SubmissionRoutes.js

--- routes/Assignments.js
+++ routes/Assignments.js
@@ -3,16 +3,34 @@ const axios = require("axios");
 const path = require("path");
 const pdf = require("html-pdf");
 const fs = require("fs");
+const pdfs = require("pdf-parse");
 
 const router = express.Router();
+
+async function getPDFTextFromURL(url) {
+  try {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const data = await pdfs(buffer);
+    return data.text;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
 const htmlToPdf = (html) => {
   return new Promise((resolve, reject) => {
     pdf
-      .create(html, { format: "Letter", timeout: 100000 ,childProcessOptions: {
-        env: {
-          OPENSSL_CONF: '/dev/null',
+      .create(html, {
+        format: "Letter",
+        timeout: 100000,
+        childProcessOptions: {
+          env: {
+            OPENSSL_CONF: "/dev/null",
+          },
         },
-      }})
+      })
       .toFile(
         path.join(__dirname, "../templates/receipt.pdf"),
         function (err, filePath) {
@@ -59,25 +77,39 @@ router.get("/courses/:courseId/assignments", async (req, res) => {
         Authorization: `${token}`,
       },
     };
-    console.log(config.url);
     const response = await axios.request(config);
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: error });
   }
 });
-
 router.post("/report-generate", async (req, res) => {
   try {
-    const {body,id} = req.body;
-    const html = `<html><body><h1>Submission Id: ${id}</h1><p>${body}</p></body></html>`;
-    const { filename } = await htmlToPdf(html);
-    const fileContent = fs.readFileSync(filename);
-
-    res.status(200).json({ fileContent });
+    const { payload } = req.body;
+    const generatedFiles = [];
+    for (const item of payload) {
+      if (item.attachments && item.attachments.length > 0) {
+        const pdfURL = item.attachments[0].url;
+        await getPDFTextFromURL(pdfURL)
+          .then(async (textNode) => {
+            const html = `<html><body><h1>Submission Id: ${item.id}</h1><p>${textNode}</p></body></html>`;
+            const { filename } = await htmlToPdf(html);
+            const fileContent = fs.readFileSync(filename);
+            generatedFiles.push(fileContent);
+          })
+          .catch((error) => {
+            res.status(500).json({ error: error });
+          });
+      } else {
+        const html = `<html><body><h1>Submission Id: ${item.id}</h1><p>${item.body}</p></body></html>`;
+        const { filename } = await htmlToPdf(html);
+        const fileContent = fs.readFileSync(filename);
+        generatedFiles.push(fileContent);
+      }
+    }
+    res.status(200).json({ generatedFiles });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: err });
   }
 });
 
